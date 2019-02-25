240f3f84552e211f6ad3280d7cb259c504db4d1d
italosvieira <italo.vieira.dev@gmail.com>
2019-02-25 20:23:24 -0300

    Adding error exception function.

--- backend/src/util/serverUtils.js
+++ backend/src/util/serverUtils.js
@@ -2,5 +2,14 @@ module.exports = {
   exit: function (msg, code, error) {
     console.log(msg, error)
     process.exit(code)
+  },
+  
+  handleError: function (ctx, status, error) {
+    console.log(error.stack)
+
+    ctx.status = status
+    ctx.body = {
+      message: error.message
+    }
   }
 }
