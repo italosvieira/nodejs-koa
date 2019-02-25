3bd58d16b322a94b5c376fc808f518378c82750d
italosvieira <italo.vieira.dev@gmail.com>
2019-02-25 20:23:40 -0300

    Adding new seeds to mongo.

--- backend/scripts/mongo/seed.js
+++ backend/scripts/mongo/seed.js
@@ -1,3 +1,30 @@
 /* eslint-disable no-undef */
 db.createUser({ user: 'fruitUser', pwd: 'fruitPassword', roles: [ 'readWrite', 'dbAdmin' ] })
-db.fruits.save({ name: 'Banana' })
+
+db.fruits.save([
+	{
+		name: 'Banana',
+		taste: 'Sweet',
+		active: true
+	},
+	{
+		name: 'Apple',
+		taste: 'Sweet',
+		active: true
+	},
+	{
+		name: 'Kiwi',
+		taste: 'Sweet',
+		active: true
+	},
+	{
+		name: 'Avocado',
+		taste: 'Like avocado?',
+		active: false
+	},
+	{
+		name: 'Pineapple',
+		taste: 'Acid',
+		active: true
+	},
+])
