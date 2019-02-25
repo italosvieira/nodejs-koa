ae46db8b8d9d180b7725a3a10927ff2ef0b23355
italosvieira <italo.vieira.dev@gmail.com>
2019-02-25 20:21:39 -0300

    Removing unused files.

---
+++ backend/src/config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Properties = require('./properties')
+const ServerUtils = require('../util/serverUtils')
+
+exports.createModel = function (name, model) {
+  mongoose.connect(Properties.mongo.url, { useNewUrlParser: true, user: Properties.mongo.user, pass: Properties.mongo.password }).then(
+    () => {
+      console.log('Connected to fruits database.')
+    },
+    error => { ServerUtils.exit('Failed to connect to fruits database.', 1, error) }
+  )
+  return mongoose.model(name, model)
+}
--- backend/src/model/Fruit.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Fruit {
-  constructor (id, name, taste, active) {
-    this.id = id
-    this.name = name
-    this.taste = taste
-    this.active = active
-  }
-}
