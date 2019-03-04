fb79cf8ca57373e084620c262a64975f5790d75e
italosvieira <italo.vieira.dev@gmail.com>
2019-03-04 17:36:38 -0300

    Removing 'useFindAndModify' from mongoose config.

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -3,6 +3,7 @@ const Properties = require('./properties')
 const ServerUtils = require('../util/serverUtils')
 
 exports.createModel = function (name, model) {
+  mongoose.set('useFindAndModify', false)
   mongoose.connect(Properties.mongo.url, { useNewUrlParser: true, user: Properties.mongo.user, pass: Properties.mongo.password }).then(
     () => {
       console.log('Connected to fruits database.')
