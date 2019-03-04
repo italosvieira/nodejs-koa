833c78b37ffc854f8f62b9de587189641ad23dc9
italosvieira <italo.vieira.dev@gmail.com>
2019-03-04 17:37:01 -0300

    Finishing fruit CRUD.

--- backend/src/service/fruit-service.js
+++ backend/src/service/fruit-service.js
@@ -11,43 +11,55 @@ const FruitModel = Mongoose.createModel('Fruit', new Schema({
 module.exports = {
   get: async function (ctx) {
     try {
-      ctx.body = await FruitModel.find()
+      ctx.body = await FruitModel.find().select(' name taste active ')
     } catch (error) {
       ServerUtils.handleError(ctx, 400, error)
     }
   },
 
   getById: async function (ctx) {
-    // TODO
+    try {
+      ctx.body = await FruitModel.findOne({ _id: ctx.params.id }).select(' name taste active ')
+    } catch (error) {
+      ServerUtils.handleError(ctx, 400, error)
+    }
   },
 
   post: async function (ctx) {
     try {
-      const x = new FruitModel({
+      const fruit = new FruitModel({
         name: ctx.request.body.name,
         taste: ctx.request.body.taste,
         active: ctx.request.body.active
       })
 
-      ctx.body = await f(await x.save())
+      ctx.body = await modelParser(await fruit.save())
     } catch (error) {
       ServerUtils.handleError(ctx, 400, error)
     }
   },
 
   put: async function (ctx) {
-    // TODO
+    try {
+      ctx.body = await modelParser(await FruitModel.findOneAndUpdate({ _id: ctx.request.body.id }, {
+        name: ctx.request.body.name,
+        taste: ctx.request.body.taste,
+        active: ctx.request.body.active
+      }, { new: true }))
+    } catch (error) {
+      ServerUtils.handleError(ctx, 400, error)
+    }
   },
 
   delete: async function (ctx) {
     try {
-      ctx.body = await f(await FruitModel.findOneAndDelete({ _id: ctx.params.id }))
+      ctx.body = await FruitModel.findOneAndDelete({ _id: ctx.params.id }).select(' name taste active ')
     } catch (error) {
       ServerUtils.handleError(ctx, 400, error)
     }
   }
 }
 
-async function f (mongoDoc) {
-  return { id: mongoDoc._id, name: mongoDoc.name, taste: mongoDoc.taste, active: mongoDoc.active }
+async function modelParser (mongoDoc) {
+  return { _id: mongoDoc._id, name: mongoDoc.name, taste: mongoDoc.taste, active: mongoDoc.active }
 }
