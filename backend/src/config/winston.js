4eec7233d49221868a78186d6f7e854641af47a3
italosvieira <italo.vieira.dev@gmail.com>
2019-03-05 19:31:11 -0300

    Configuring winston logger.

--- backend/src/config/winston.js
+++ backend/src/config/winston.js
@@ -1,31 +1,21 @@
 const winston = require('winston')
 
-const config = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    verbose: 3,
-    debug: 4
-  },
-  colors: {
-    error: 'red',
-    warn: 'yellow',
-    info: 'green',
-    verbose: 'cyan',
-    debug: 'blue'
-  }
-}
-
-winston.addColors(config.colors)
-
 module.exports = winston.createLogger({
-  levels: config.levels,
+  level: 'info',
   format: winston.format.combine(
-    winston.format.colorize({ all: true }),
-    winston.format.simple()
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(info => transform(info))
   ),
   transports: [
     new winston.transports.Console()
   ]
 })
+
+function transform (msg) {
+  if (msg.stack) {
+    return `${msg.timestamp} ${msg.level}: ${msg.message} \n ${msg.stack}`
+  }
+  return `${msg.timestamp} ${msg.level}: ${msg.message}`
+}
