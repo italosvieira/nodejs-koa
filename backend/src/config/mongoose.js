64d1506e99aa77f2166704fe53af4140607597df
italosvieira <italo.vieira.dev@gmail.com>
2019-03-04 22:34:57 -0300

    Changing console.log to winston calls.

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 const Properties = require('./properties')
 const ServerUtils = require('../util/serverUtils')
+const logger = require('./winston')
 
 exports.createModel = function (name, model) {
   mongoose.set('useFindAndModify', false)
   mongoose.connect(Properties.mongo.url, { useNewUrlParser: true, user: Properties.mongo.user, pass: Properties.mongo.password }).then(
     () => {
-      console.log('Connected to fruits database.')
+      logger.log('info', 'Connected to fruits database.')
     },
     error => { ServerUtils.exit('Failed to connect to fruits database.', 1, error) }
   )
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,26 @@
 const Koa = require('koa')
-const Logger = require('koa-logger')
+const koaLogger = require('koa-logger')
 const Helmet = require('koa-helmet')
 const Cors = require('@koa/cors')
 const BodyParser = require('koa-bodyparser')
+const logger = require('./config/winston')
 
 const Properties = require('./config/properties')
 const Router = require('./routes/router')
 
 if (Properties.isServerPropertiesInvalid) {
-  console.log('Server Properties invalid.')
+  logger.log('error', 'Server Properties invalid.')
   process.exit(1)
 }
 
 const koa = new Koa()
-koa.use(Logger())
+koa.use(koaLogger())
 koa.use(Helmet())
 koa.use(Cors())
 koa.use(BodyParser())
 koa.use(Router.routes())
 koa.use(Router.allowedMethods())
 
-koa.listen(Properties.port, () => console.log(`Server running on port ${Properties.port}`))
+koa.listen(Properties.port, () => logger.log('info', `Server running on port ${Properties.port}`))
 
 module.exports = koa
