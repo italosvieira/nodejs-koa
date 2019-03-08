702c459c7d11dca1945e7590a8262b12dcae08a4
italosvieira <italo.vieira.dev@gmail.com>
2019-03-08 19:25:03 -0300

    Fixing generic exception handler.

--- backend/src/util/exceptionHandler.js
+++ backend/src/util/exceptionHandler.js
@@ -1,10 +1,15 @@
+const BusinessException = require('../exception/businessException')
+const ValidationError = require('mongoose').Error.ValidationError
 const logger = require('../config/winston')
 
-module.exports = function (ctx, status, error) {
-  logger.log('error', error)
+module.exports = function (ctx, error) {
+  logger.log('error', error.message, error)
 
-  ctx.status = status
-  ctx.body = {
-    message: error.message
+  if (error instanceof BusinessException) {
+    ctx.throw(error.httpStatusCode, error.message)
+  } else if (error instanceof ValidationError) {
+    ctx.throw(400, error.message)
+  } else {
+    ctx.throw(500, error.message)
   }
 }
