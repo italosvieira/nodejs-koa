fcd577f19f1d75e9b5b7f44494973891009c9d83
italosvieira <italo.vieira.dev@gmail.com>
2019-03-12 14:28:36 -0300

    Adding properties of asymmetric keys.

--- backend/src/config/properties.js
+++ backend/src/config/properties.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const isNullOrUndefined = require('../util/isNullOrUndefined')
 
 const nodeEnv = process.env.NODE_ENV
@@ -5,7 +6,8 @@ const isDev = nodeEnv === 'dev'
 const isProd = nodeEnv === 'prod'
 
 module.exports = {
-  isServerPropertiesInvalid: isNullOrUndefined(nodeEnv) || isNullOrUndefined(process.env.MONGO_URL) || isNullOrUndefined(process.env.MONGO_USER) || isNullOrUndefined(process.env.MONGO_PASSWORD),
+  isServerPropertiesInvalid: isNullOrUndefined(nodeEnv) || isNullOrUndefined(process.env.PATH_ASYMMETRIC_PRIVATE_KEY) || isNullOrUndefined(process.env.PATH_ASYMMETRIC_PUBLIC_KEY) ||
+    isNullOrUndefined(process.env.MONGO_URL) || isNullOrUndefined(process.env.MONGO_USER) || isNullOrUndefined(process.env.MONGO_PASSWORD),
 
   port: process.env.NODE_PORT || 3000,
 
@@ -18,5 +20,10 @@ module.exports = {
     url: process.env.MONGO_URL,
     user: process.env.MONGO_USER,
     password: process.env.MONGO_PASSWORD
+  },
+
+  loadAsymmetricKeys: function () {
+    process.env.ASYMMETRIC_PRIVATE_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PRIVATE_KEY).toString()
+    process.env.ASYMMETRIC_PUBLIC_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PUBLIC_KEY).toString()
   }
 }
