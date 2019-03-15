ad281a3c6d5f1e1810082239470611e9b407cc12
italosvieira <italo.vieira.dev@gmail.com>
2019-03-15 00:36:43 -0300

    Refactoring server file and adding TODOs.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,36 @@
 const Koa = require('koa')
-const koaHelmet = require('koa-helmet')
 const koaCors = require('@koa/cors')
+const koaHelmet = require('koa-helmet')
 const koaBodyParser = require('koa-bodyparser')
-const Jwt = require('koa-jwt')
 
-const properties = require('./config/properties')
+const Mongoose = require('./config/mongoose')
+const AsymmetricKeysLoader = require('./config/asymmetricKeysLoader')
+const EnvironmentVariablesValidator = require('./config/environmentVariablesValidator')
+
+const logger = require('./config/winston')
 const publicRoutes = require('./routes/public-router')
 const privateRoutes = require('./routes/private-router')
-const logger = require('./config/winston')
-const exit = require('./utils/exit')
-const SecurityExceptionHandler = require('./handlers/securityExceptionHandler')
 
-properties.loadAsymmetricKeys()
+async function start () {
+  AsymmetricKeysLoader()
+  EnvironmentVariablesValidator()
+  await Mongoose()
 
-if (properties.isServerPropertiesInvalid) {
-  exit('Server Properties invalid.', 1)
+  // Middleware is called on each request. Stuff that is loaded only once or is needed on middleware needs to be loaded prior. Remember to gzip stuff
+  const app = new Koa()
+  // Logger and respondetime middleware here.
+  app.use(koaCors())
+  app.use(koaHelmet())
+  app.use(koaBodyParser())
+  // Security validator middleware here
+  /* app.use(SecurityExceptionHandler()) */
+  app.use(publicRoutes.routes())
+  app.use(publicRoutes.allowedMethods())
+  // JWT middleware here.
+  /* app.use(Jwt({ secret: Buffer.from(process.env.ASYMMETRIC_PUBLIC_KEY) }, { algorithm: 'RS256' })) */
+  app.use(privateRoutes.routes())
+  app.use(privateRoutes.allowedMethods())
+  app.listen(process.env.NODE_PORT || 3000, () => logger.info(`Application running on port ${process.env.NODE_PORT || 3000}.`))
 }
 
-const app = new Koa()
-app.use(koaHelmet())
-app.use(koaCors())
-app.use(koaBodyParser())
-app.use(SecurityExceptionHandler())
-app.use(publicRoutes.routes())
-app.use(publicRoutes.allowedMethods())
-app.use(Jwt({ secret: Buffer.from(process.env.ASYMMETRIC_PUBLIC_KEY) }, { algorithm: 'RS256' }))
-app.use(privateRoutes.routes())
-app.use(privateRoutes.allowedMethods())
-app.listen(properties.port, () => logger.log('info', `Application running on port ${properties.port}.`))
-
-module.exports = app
+start()
