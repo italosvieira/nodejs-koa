0b97ed5ca6250daea685fb4f256d889138de48b5
italosvieira <italo.vieira.dev@gmail.com>
2019-03-14 23:38:57 -0300

    changing log format.

--- backend/src/config/winston.js
+++ backend/src/config/winston.js
@@ -3,19 +3,15 @@ const winston = require('winston')
 module.exports = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
+    winston.format(info => {
+      info.level = info.level.toUpperCase()
+      return info
+    })(),
     winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.align(),
-    winston.format.printf(info => transform(info))
+    winston.format.printf(({ level, message, timestamp, stack }) => {
+      return stack ? `[${timestamp}] ${level}: ${message} \n ${stack}` : `[${timestamp}] ${level}: ${message}`
+    })
   ),
-  transports: [
-    new winston.transports.Console()
-  ]
+  transports: [ new winston.transports.Console() ]
 })
-
-function transform (msg) {
-  if (msg.stack) {
-    return `${msg.timestamp} ${msg.level}: ${msg.message} \n ${msg.stack}`
-  }
-  return `${msg.timestamp} ${msg.level}: ${msg.message}`
-}
