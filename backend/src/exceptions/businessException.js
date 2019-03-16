9512e3500708d1b25cd2ab71c528f9458c477494
italosvieira <italo.vieira.dev@gmail.com>
2019-03-16 00:31:08 -0300

    Changing businessexception to hold client message and loggin message.

--- backend/src/exceptions/businessException.js
+++ backend/src/exceptions/businessException.js
@@ -1,6 +1,7 @@
 module.exports = class BusinessException {
-  constructor (message, httpStatusCode) {
+  constructor (message, clientMessage, httpStatusCode) {
     this.message = message
+    this.clientMessage = clientMessage
     this.httpStatusCode = httpStatusCode
     this.stack = (new Error()).stack
   }
--- backend/src/utils/validateMongoId.js
+++ backend/src/utils/validateMongoId.js
@@ -3,10 +3,10 @@ const isMongoId = require('validator/lib/isMongoId')
 
 module.exports = function (id) {
   if (!id) {
-    throw new BusinessException('Parameter id must not be null.', 400)
+    throw new BusinessException(`Parameter id invalid. id: ${id}`, 'Parameter id must not be null.', 400)
   }
 
   if (!isMongoId(id)) {
-    throw new BusinessException('Parameter id is not a valid mongo id.', 400)
+    throw new BusinessException(`Parameter id is not a valid mongo id.: id: ${id}`, 'Parameter id is not a valid mongo id.', 400)
   }
 }
