359d12813c56f755542624baad10924f54d592af
italosvieira <italo.vieira.dev@gmail.com>
2019-03-19 21:51:08 -0300

    Adding logic to exit application on when there is no asymmetric keys.

--- src/config/asymmetricKeysLoader.js
+++ src/config/asymmetricKeysLoader.js
@@ -1,6 +1,18 @@
 const fs = require('fs')
+const logger = require('./winston')
 
 module.exports = function () {
-  process.env.ASYMMETRIC_PRIVATE_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PRIVATE_KEY).toString()
-  process.env.ASYMMETRIC_PUBLIC_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PUBLIC_KEY).toString()
+  try {
+    process.env.ASYMMETRIC_PRIVATE_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PRIVATE_KEY).toString()
+  } catch (e) {
+    logger.error('Could not load asymmetric private key file.')
+    process.exit(1)
+  }
+
+  try {
+    process.env.ASYMMETRIC_PUBLIC_KEY = fs.readFileSync(process.env.PATH_ASYMMETRIC_PUBLIC_KEY).toString()
+  } catch (e) {
+    logger.error('Could not load asymmetric public key file.')
+    process.exit(1)
+  }
 }
